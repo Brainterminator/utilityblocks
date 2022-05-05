07d7b5ab267fa3823543c33c6c9dd94f642e2b8c
Brainterminator <thebrainterminator@gmail.com>
2022-05-06 01:21:58 +0200

    Added Utilium Fragments

--- src/main/java/net/brain/utilityblocks/item/ModItems.java
+++ src/main/java/net/brain/utilityblocks/item/ModItems.java
@@ -15,7 +15,8 @@ public class ModItems {
     public static final RegistryObject<Item> UTILIUM = ITEMS.register("utilium",
             () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
-
+    public static final RegistryObject<Item> UTILIUM_FRAGMENTS = ITEMS.register("utilium_fragments",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
