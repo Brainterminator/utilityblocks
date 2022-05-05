4275b41a7055f0bf94da35fa0d5fff1809f4c6c6
Brainterminator <thebrainterminator@gmail.com>
2022-05-06 01:12:14 +0200

    Added Utilium

---
+++ src/main/java/net/brain/utilityblocks/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.brain.utilityblocks.item;
+
+import net.brain.utilityblocks.UtilityBlocks;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, UtilityBlocks.MOD_ID);
+
+    public static final RegistryObject<Item> UTILIUM = ITEMS.register("utilium",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
